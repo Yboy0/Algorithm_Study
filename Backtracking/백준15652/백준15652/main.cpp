3386673f3d1c64e89ad289683ee6faa6a3f668dc
Oo-Uu <fbduddn97@naver.com>
2021-02-01 18:42:41 +0900

    백준15652(backtracking) add

--- Backtracking/백준15652/백준15652/main.cpp
+++ Backtracking/백준15652/백준15652/main.cpp
@@ -1,17 +1,16 @@
 //
 //  main.cpp
-//  백준15651
+//  백준15652
 //
 //  Created by 유영우 on 2021/02/01.
 //
 
 #include <iostream>
 using namespace std;
+
 int n,m;
 int cnt;
-int arr[8];
-//int check[10];
-
+int arr[9];
 void sequence(int cnt){
     if(cnt == m){
         for(int i=0; i<m; i++){
@@ -21,8 +20,14 @@ void sequence(int cnt){
     }
     else{
         for(int i=1; i<n+1; i++){
-            arr[cnt] = i;
-            sequence(cnt+1);
+            if(cnt==0){
+                arr[cnt]=i;
+                sequence(cnt+1);
+            }
+            if(cnt>0 && arr[cnt-1] <= i){
+                arr[cnt]=i;
+                sequence(cnt+1);
+            }
         }
     }
 }
@@ -30,5 +35,6 @@ void sequence(int cnt){
 int main(void) {
     scanf("%d %d",&n,&m);
     sequence(cnt);
+   
     return 0;
 }
