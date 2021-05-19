c79630dddd630e8d3f4e6187edd0e1873603078d
Oo-Uu <fbduddn97@naver.com>
2021-05-19 23:46:46 +0900

    !!백준12100(2048Easy)

--- Binary Search/백준2110(공유기 설치)/백준2110(공유기 설치)/main.cpp
+++ Binary Search/백준2110(공유기 설치)/백준2110(공유기 설치)/main.cpp
@@ -7,44 +7,86 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
+//집 n개:
+// 집에 공유기 c개 설치 해야 한다
+// 한 집에 공유기 하나 설치 가능
+// 가장 인접한 두 공유기 사이의 거리 크게 설치해야 함
+// c개의 공유기를 n개의 집에 적당히 설치해야 한다.
+// 두 공유기 사이의 거리를 최대로 하는 프로그램을 작성하시오
+// n: 집의 개수 c: 공유기 개수
 
-int n; // 집의 개수
-int c; // 공유기 개수
-// 가장 인접한 두 공유기 사이의 거리를 최대로
-int house[200001];
-int result;
+int n,c;
+int house;
+vector<int>houses;
+int low,mid,high;
+vector<int>modemHouses; //houses의 index를 넣기
+int cnt;
+int dist;
 
-void modem(int begin, int end, int cnt){
-    
-    int mid = (end-(begin))/2;
-    
-    if(cnt<=3){
-        result = min(house[end]-house[mid],house[mid]-house[begin]);
-    }else{
-        if(house[end]-house[mid]>house[mid]-house[begin]){
-            modem(mid,end,cnt-(cnt/2));
-        }else{
-            modem(begin,mid,cnt-(cnt/2));
+
+bool compare(int a, int b){
+    return a<b;
+}
+
+void modem(){
+
+    while(low<high){
+        
+        sort(modemHouses.begin(),modemHouses.end(),compare);
+        
+        if(c==cnt){
+            break;
         }
+        
+        int dis=0;
+        for(unsigned i=1; i<modemHouses.size(); i++){
+            if(houses[modemHouses[i]]-houses[modemHouses[i-1]]>dis){
+                dis =houses[modemHouses[i]]-houses[modemHouses[i-1]];
+                low = modemHouses[i-1];
+                high = modemHouses[i];
+            }
+        }
+        
+        mid = (low+high)/2;
+//        cout<< "high:"<<" "<< high << " "<< "low:" <<" "<<low<<" " << "mid:" << " " << mid << endl;
+        modemHouses.push_back(mid);
+        cnt++;
+        
     }
     
     
 }
 
 
+
 int main(void) {
-    
     cin >> n >> c;
-    
     for(int i=0; i<n; i++){
-        cin >> house[i];
+        cin >> house;
+        houses.push_back(house);
     }
     
-    sort(house,house+n);
-    modem(0,n-1,c);
+    sort(houses.begin(),houses.end(),compare);
     
-    cout << result;
-    return 0;
+    low=0;
+    high=n-1;
+    modemHouses.push_back(low);
+    modemHouses.push_back(high);
+    cnt+=2;
+    dist = houses[high] - houses[low];
+    
+    
+    modem();
+    
+    for(unsigned i=1; i<modemHouses.size(); i++){
+        if(houses[modemHouses[i]]-houses[modemHouses[i-1]]<dist){
+            dist =houses[modemHouses[i]]-houses[modemHouses[i-1]];
+        }
+    }
+    
+    cout << dist;
+   
 }
--- Simulation/백준12100(2048Easy)/백준12100(2048Easy)/main.cpp
+++ Simulation/백준12100(2048Easy)/백준12100(2048Easy)/main.cpp
@@ -6,21 +6,41 @@
 //
 
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-// 한 번의 이동은 보드 위에 있는 전체 블록을 상하좌우 네 방향 중 하나로 이동시키는 것
-// 같은 값을 갖는 두 블록이 충돌하면 두 블록은 하나로 합쳐짐
-// 한 번의 이동에서  이미 합쳐진 블록은 또 다른 블록과 다시 합쳐질 수 없다
+// 전체 블록을 상하좌우 네 방향 중 하나로 이동시키는 것
+// 같은 값을 갖는 두 블록이 충돌하면 두 블록은 하나로 합쳐진다.
+// 한 번의 이동에서 이미 합쳐진 블록은 또 다른 블록과 다시 합쳐질 수 없다
+// 보드의 크기와 보드판의 블록 상태가 주어졌을 때,
+//  최대 5번 이동해서 만들 수 있는 가장 큰 블록의 값을 구하라
 
-// 최대 5번 이동해서 만들 수 있는 가장 큰 블록의 값을 구해라
+//0은 빈칸, 나머지 값은 블록
+int n;
+int num;
+int result;
 
-//0: 빈칸
-//나머지: 블록 (2~1024)
-
-
-int main(void) {
+void block(int cnt, int arrr[21][21]){
 
     
+    
+}
+
+int main(void) {
+    
+    int arr[21][21];
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> num;
+            arr[i][j]=num;
+        }
+    }
+    
+    
+    block(0,arr);
+    
+    cout << result;
     return 0;
 }
